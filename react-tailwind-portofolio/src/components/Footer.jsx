4f4e04feb58d7b2172cd8d9cebe16966275376c0
rosa ap <7266099-rosa.ap@users.noreply.gitlab.com>
2022-12-29 09:48:53 +0700

    (Feat) Portofolio web with react and tailwind css

---
+++ react-tailwind-portofolio/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="bg-gray-800 text-sm p-4 text-center text-white">
+    Copyright © 2022 Rosa Arimbi.All Rights reserved.
+    </div>
+};
+
+export default Footer;
